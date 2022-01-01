87fc9942c88e265e9975784755a8e3d0465db97d
naveen <naveenreddy.k07@gmail.com>
2022-01-01 11:48:14 +0530

    EmployeeWageProblems

---
+++ Employ/EmployeeWage.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employ
+{
+    public class EmployeeWage
+    {
+        public const int isPartTime = 1;
+        public const int isFullTime = 2;
+        public const int empWagePerHour = 20;
+        public const int maxWorkingDaysInMonth = 20;
+        public const int maxHoursInMonth = 100;
+        public const int empRatePerDay = 20;
+
+        public void AddSwitch()
+        {
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(1, 2);
+
+            //UC4
+            switch (empCheck)
+            {
+                case isPartTime:
+                    empHrs = 8;
+                break;
+
+                case isFullTime:
+                    empHrs = 4;
+                break;
+
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * empWagePerHour;
+            Console.WriteLine("Daily EmpWage:" + empWage);
+
+            //UC5
+            int totalWorkingHrs = 0;
+            int workingHrs = 0;
+            int empWagePerMonth = 0;
+            switch (empCheck)
+            {
+                case isPartTime:
+                    workingHrs = 8;
+                    break;
+
+                case isFullTime:
+                    workingHrs = 4;
+                    break;
+
+                default:
+                    workingHrs = 0;
+                    break;
+            }
+            totalWorkingHrs = workingHrs * maxHoursInMonth;
+            empWagePerMonth = totalWorkingHrs * empRatePerDay;
+            Console.WriteLine("wages Per Month:" + empWagePerMonth);
+
+            //UC6
+            int totalWorkingDaysPerMonth = 0;
+            switch (empCheck)
+            {
+                case isPartTime:
+                    workingHrs = 8;
+                    break;
+
+                case isFullTime:
+                    workingHrs = 4;
+                    break;
+
+                default:
+                    workingHrs = 0;
+                    break;
+            }
+            totalWorkingHrs = workingHrs + maxHoursInMonth;
+            totalWorkingDaysPerMonth = totalWorkingHrs * maxWorkingDaysInMonth;
+            Console.WriteLine("wages Per Month:" + totalWorkingDaysPerMonth);
+        }
+    }
+}
---
+++ Employ/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Employ
+{
+    public class Program
+    {
+        public const int isPartTime = 1;
+        public const int isFullTime = 1;
+        public const int empWagePerHour = 20;
+        static void Main(string[] args)
+        {
+            EmployeeWage emp = new EmployeeWage();
+            emp.AddSwitch();
+
+            //UC1
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if(empCheck== isFullTime)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+            //UC2
+            int empHrs = 0;
+            int empWage = 0;
+            if(empCheck== isFullTime)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * empWagePerHour;
+            Console.WriteLine("Daily EmpWage:" + empWage);
+
+            //UC3
+            int partTimeHrs = 0;
+            int partTimeEmpWage = 0;
+            if (empCheck == isPartTime)
+            {
+                partTimeHrs = 8;
+            }
+            else
+            {
+                partTimeHrs = 0;
+            }
+            partTimeEmpWage = partTimeHrs * empWagePerHour;
+            Console.WriteLine("PartTime EmpWage:" + partTimeEmpWage);
+        }
+    }
+}
